c9b086a5cfd79eaa93b0964b990e133bc3248a3a
gsloan01 <60530127+gsloan01@users.noreply.github.com>
2021-05-17 14:32:03 -0600

    Started making some datas

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level", menuName = "Data/LevelData")]
+public class Level : ScriptableObject
+{
+    public int totalThreat;
+}
